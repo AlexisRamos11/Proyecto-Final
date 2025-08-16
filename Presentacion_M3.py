d839e6da732b699e7bc1d24cbb970415bf30718b
AlexisRamos11 <65379829+AlexisRamos11@users.noreply.github.com>
2025-08-15 20:18:37 -0600

    Add files via upload

---
+++ Presentacion_M3.py
@@ -0,0 +1,46 @@
+import warnings
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+from sklearn import set_config
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
+
+# Tema Principal
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+import streamlit as st
+
+
+random_seed = 333  # Semilla para reproducibilidad de resultados
+np.random.seed(random_seed)  # Para reproducibilidad
+
+# Configuración de opciones de visualización para pandas
+pd.set_option('display.max_columns', None)  # Muestra todas las columnas
+pd.set_option('display.max_rows', 15)  # Ajusta el número de filas a mostrar
+
+# Configuraciones extras
+sns.set_style('dark')
+dark_template = pio.templates['plotly_dark'].to_plotly_json()
+dark_template['layout']['paper_bgcolor'] = 'rgba(30, 30, 30, 0.5)'
+dark_template['layout']['plot_bgcolor'] = 'rgba(30, 30, 30, 0)'
+pio.templates['plotly_dark_semi_transparent'] = go.layout.Template(dark_template)
+pio.templates.default = 'plotly_dark_semi_transparent'
+set_config(transform_output="pandas")
+set_config(display='diagram')
+warnings.filterwarnings("ignore")
+#%matplotlib inline
+
+
+#lectura de datos
+df=pd.read_csv('winequality-red.csv',encoding='latin-1')
+st.sidebar.subheader('Ramos Palacios Juan Alexis')
+ejercicio=st.sidebar.ratio('Seleccione la opción',['Introducción','Análisis de Agrupamiento',
+                                        'Selección del Modelo','Perfilamiento','Conclusiones'])
